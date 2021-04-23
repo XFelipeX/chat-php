a53bcaba620abbdb3d3912f0ee6cbe2ec748d366
Felipe <felipediasamorimpessoa@gmail.com>
2021-04-23 16:39:42 -0300

    init project

---
+++ ChatWeb/script.js
@@ -0,0 +1,12 @@
+var conn = new WebSocket('ws://localhost:8080');
+conn.onopen = function (e) {
+  console.log('Connection established!');
+};
+
+conn.onmessage = function (e) {
+  console.log(e.data);
+};
+
+conn.onerror = (e) => {
+  console.log(e);
+};
